675cbb8b7ff081cd99d20ebeeae835887e9c5d00
toxir-brat <toxirli94@gmail.com>
2022-09-08 11:42:58 +0500

    olg'a

---
+++ src/modules/public/tenants/tenant.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateTenantDto } from './dto/create-tenant.dto';
+import { TenantEntity } from './entities/tenant.entity';
+import { TenantService } from './tenant.service';
+
+@ApiTags('tenant')
+@Controller('tenant')
+export class TenantsController {
+  constructor(private readonly tenantService: TenantService) {}
+
+  @Post()
+  create(@Body() createTenantDto: CreateTenantDto): Promise<TenantEntity> {
+    console.log(createTenantDto);
+    
+    return this.tenantService.create(createTenantDto);
+  }
+
+  @Get()
+  findAll(): Promise<TenantEntity[]> {
+    return this.tenantService.findAll();
+  }
+}
---
+++ src/orm.config.ts
@@ -0,0 +1,12 @@
+import { join } from 'path';
+import config from './config';
+
+module.exports = {
+  type: 'postgres',
+  url: config.ENV ? config.DB_URL_TEST : config.DB_URL,
+  synchronize: true,
+  autoLoadEntities: true,
+  logger: 'debug',
+  entities: [join(__dirname, './modules/public/**/entities/*.entity{.ts,.js}')],
+  migrations: [join(__dirname, './migrations/*{.ts,.js}')],
+};
